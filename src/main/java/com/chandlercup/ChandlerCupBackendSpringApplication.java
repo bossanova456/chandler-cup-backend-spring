0cad242e54b50bc243cc80983a2918aab0d7db0d
Andrews, Taylor <taylor.andrews0391@gmail.com>
2023-11-09 11:37:13 -0500

    Initial commit

---
+++ src/main/java/com/chandlercup/ChandlerCupBackendSpringApplication.java
@@ -0,0 +1,13 @@
+package com.chandlercup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChandlerCupBackendSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChandlerCupBackendSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chandlercup/ChandlerCupBackendSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chandlercup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChandlerCupBackendSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
